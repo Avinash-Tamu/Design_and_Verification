77c16eadfa50eff0d3af85f6ba28a0b314230725
Avinash-Tamu <avinsing7@tamu.edu>
2024-12-24 18:26:26 -0600

    Update spi_top_testbench.sv

--- SPI_UVM_VERIFICATION/tb/spi_top_testbench.sv
+++ SPI_UVM_VERIFICATION/tb/spi_top_testbench.sv
@@ -12,56 +12,36 @@ module top_tb;
    import uvm_pkg::*;
 `include "uvm_macros.svh"
 import spi_pkg::*;
-`include "spi_sequence_item.sv"
-`include "spi_sequence.sv"
-`include "spi_driver.sv"
-`include "spi_interface.sv"
-`include "spi_monitor.sv"
-`include "spi_agent.sv"
-`include "spi_scorecard.sv"
-`include "spi_environment.sv"
-`include "spi_test.sv"
-`include "spi_master.v"
-`include "spi_slave.v"
-  //---------------------------------------
-  //clock and reset signal declaration
-  //---------------------------------------
+
+
   bit mclk;
   bit reset;
-  
-  //---------------------------------------
-  //clock generation
-  //---------------------------------------
+
   always #5 mclk = ~mclk;
   
-  //---------------------------------------
-  //reset Generation
-  //---------------------------------------
+
   initial begin
+    mclk = 0;
     reset = 0;
     #5 reset =1;
   end
   
-  //---------------------------------------
-  //interface instance
-  //---------------------------------------
-  spi_interface intf(mclk,reset);
+
+   spi_interface intf(mclk,reset);
   
-  //---------------------------------------
-  //DUT instance
-  //---------------------------------------
+
   top_dut dut(intf.mclk, intf.reset,intf.load_master,intf.load_slave,intf.read_master,
-  intf.read_slave,intf.start,intf.data_in_master,intf.data_in_slave,
-  
-              intf.data_out_master,intf.data_out_slave);
+  intf.read_slave,intf.start,intf.data_in_master,intf.data_in_slave,intf.data_out_master,intf.data_out_slave);
   
    initial begin 
-     uvm_config_db#(virtual spi_interface)::set(null,"*","vif",intf);
-    //enable wave dump
-    $dumpfile("dump.vcd"); 
-    $dumpvars;
+     uvm_config_db#(virtual interface spi_interface)::set(null,"*","vif",intf);
   end
-  
+ initial begin
+
+        #500;
+        //$display("Data out master: %d", intf.data_out_master);
+        //$display("Data out slave: %d", intf.data_out_slave);
+    end
   //---------------------------------------
   //calling test
   //---------------------------------------
